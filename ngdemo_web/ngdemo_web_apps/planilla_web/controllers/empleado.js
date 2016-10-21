7b375851a55fbbc019436a5665fe8ba43645ef18
tu nombre <tumail@gmail.com>
2016-10-20 19:05:10 -0500

    jue20 10 sub4

--- ngdemo_web/ngdemo_web_apps/planilla_web/controllers/empleado.js
+++ ngdemo_web/ngdemo_web_apps/planilla_web/controllers/empleado.js
@@ -1,16 +1,68 @@
 app
 
-.controller('EmpleadoCtr', function ($scope, planillaService) {
+    .controller('EmpleadoCtr', function($scope, $window, planillaService) {
 
-	$scope.lista = [];
-	
-	planillaService.Empleado.query().$promise.then(function (r) {
-		$scope.lista = r;
-	}, function (err) {
-		console.log("Hay error");
-	});
+    $scope.empleado = {};
+    $scope.lista = [];
 
-    $scope.save = function  () {
-        
+    $scope.listar = function() {
+        planillaService.Empleado.query().$promise.then(function(r) {
+            $scope.lista = r;
+        }, function(err) {
+            console.log("Hay error");
+        });
     };
+    $scope.listar();
+
+    $scope.sel = function(d) {
+        //$scope.empleado = d;
+
+        planillaService.Empleado.get({ id: d.id }).$promise.then(function(r) {
+            $scope.empleado = r;
+        }, function(err) {
+            console.log("Hay error");
+        });
+
+    };
+
+    $scope.save = function() {
+
+        if ($scope.empleado.id) {
+            planillaService.Empleado.update({ id: $scope.empleado.id }, $scope.empleado).$promise.then(function(r) {
+                console.log("Editó " + r);
+
+                $scope.listar();
+
+            }, function(err) {
+                console.log("Hay error");
+            });
+
+        } else {
+            planillaService.Empleado.save($scope.empleado).$promise.then(function(r) {
+                console.log("Insertó " + r);
+
+                $scope.listar();
+
+            }, function(err) {
+                console.log("Hay error");
+            });
+        }
+
+
+    };
+
+    $scope.delete = function(d) {
+        if ($window.confirm("Está seguro?")) {
+            planillaService.Empleado.delete({ id: d.id }).$promise.then(function(r) {
+                console.log("Eliminado " + r);
+
+                $scope.listar();
+
+            }, function(err) {
+                console.log("Hay error");
+            });
+        };
+    };
+
+
 });
--- ngdemo_web/ngdemo_web_apps/planilla_web/services/planillaService.js
+++ ngdemo_web/ngdemo_web_apps/planilla_web/services/planillaService.js
@@ -3,11 +3,11 @@ app
     .factory('planillaService', function($resource) {
     var url = 'http://localhost:8000/api/planilla/';
     return {
-        Empleado: $resource(url + 'empleados/:id', { id: '@id' }, {
+        Empleado: $resource(url + 'empleados/:id/', { id: '@id' }, {
             'update': { method: 'PUT' },
 
         }),
-        Empresa: $resource(url + '/empresas/:id', { id: '@id' }, {
+        Empresa: $resource(url + '/empresas/:id/', { id: '@id' }, {
             'update': { method: 'PUT' }
         }),
 

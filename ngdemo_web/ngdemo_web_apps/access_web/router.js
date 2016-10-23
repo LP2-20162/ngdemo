ec63e9b3a71c5fa90c6068b3e258933ecf3168c0
tu nombre <tumail@gmail.com>
2016-10-23 12:19:16 -0500

    login sin localstore

---
+++ ngdemo_web/ngdemo_web_apps/access_web/router.js
@@ -0,0 +1,16 @@
+app
+
+//algo
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+
+
+
+    .state('login', {
+      url: "/login",
+      templateUrl: "ngdemo_web_apps/access_web/views/login/index.html"
+    });
+
+});
